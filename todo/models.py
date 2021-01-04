b1197e89111b2304831b7ffc7f534b748ca638d2
Github22799 <31520065+Github22799@users.noreply.github.com>
2021-01-05 00:38:54 +0200

    initial model for the todos

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=200)
+    memo = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_completed = models.DateTimeField()
+    important = models.BooleanField(default=False)
+
