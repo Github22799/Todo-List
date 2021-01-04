50d2b2f927ad77b35fbf307c10277cf4e45d3db0
Github22799 <31520065+Github22799@users.noreply.github.com>
2021-01-05 00:53:10 +0200

    added the forign key of the user to the Todo model and the migrations are made.

--- todo/models.py
+++ todo/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Todo(models.Model):
@@ -7,3 +8,4 @@ class Todo(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     date_completed = models.DateTimeField()
     important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
