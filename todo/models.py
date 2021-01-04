a9a627d7ee766f7e14662fc26c60c4ef171a66e5
Github22799 <31520065+Github22799@users.noreply.github.com>
2021-01-05 00:48:56 +0200

    added the app 'todo' to the installed apps, created a model for the Todo object, registered it in the admin site and made the migrations.

--- todo/models.py
+++ todo/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
+
 class Todo(models.Model):
     title = models.CharField(max_length=200)
     memo = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
     date_completed = models.DateTimeField()
     important = models.BooleanField(default=False)
-
